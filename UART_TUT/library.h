78e922b6bddb7d5eac28b280c9efcdd402464d2f
unknown <dat.ho1801@live.com>
2015-09-25 23:51:22 +0700

    no message

--- UART_TUT/library.h
+++ UART_TUT/library.h
@@ -9,7 +9,22 @@
 #ifndef LIBRARY_H_
 #define LIBRARY_H_
 
-
+#include <stdint.h>
+#include <stdbool.h>
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/flash.h"
+#include "inc/hw_gpio.h"
+#include "inc/hw_sysctl.h"
+#include "inc/hw_ints.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_uart.h"
+#include "driverlib/debug.h"
+#include "driverlib/rom.h"
+#include "driverlib/uart.h"
+#include "driverlib/pin_map.h"
 
 
 
--- UART_TUT/main.c
+++ UART_TUT/main.c
@@ -5,7 +5,21 @@
  *      Author: Dat Ho
  *      Email : dat.ho1801@live.com
  ******************************************************************************/
+
+#include "library.h"
+
 int main(void) {
-	
-	return 0;
+    SysCtlClockSet(
+            SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ
+                    | SYSCTL_OSC_MAIN);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_1 | GPIO_PIN_1);
+    UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200, (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+
+    while (1) {
+
+    }
 }
